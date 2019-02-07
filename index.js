84e14d0928fd4b2fe6c195a2a4a16d1eb2143f6b
Emilien Devos <contact@emiliendevos.be>
2019-02-07 15:45:37 +0100

    rework with request module

--- index.js
+++ index.js
@@ -1,46 +1,54 @@
 const jsonServer = require('json-server');
-const getJSON = require('get-json');
+const request = require('request');
 const parsedJSON = require('./db.json');
 const server = jsonServer.create();
 const router = jsonServer.router(parsedJSON);
 const middlewares = jsonServer.defaults({ readOnly: true, noCors: true, logger: false });
 
+const options = {
+    url: 'https://cloudquery.unixfox.eu/query?url=https%3A%2F%2Fstratus.network%2Fplay&selectors=*:nth-child(5)%20*:nth-child(2)%20%3E%20*:nth-child(1)%20%3E%20*%20%3E%20*%20%3E%20*,*:nth-child(2)%20%3E%20*:nth-child(5)%20%3E%20*%20%3E%20*%20%3E%20*:nth-child(1)%20%3E%20*:nth-child(2)',
+    json: true
+}
+
 var mapName;
 var playersNumber;
 
 server.use(jsonServer.bodyParser);
-getJSON('https://cloudquery-phjvnjk3w.now.sh/query?url=https%3A%2F%2Fstratus.network%2Fplay&selectors=*:nth-child(5)%20*:nth-child(2)%20%3E%20*:nth-child(1)%20%3E%20*%20%3E%20*%20%3E%20*,*:nth-child(2)%20%3E%20*:nth-child(5)%20%3E%20*%20%3E%20*%20%3E%20*:nth-child(1)%20%3E%20*:nth-child(2)', function (error, response) {
+server.get('/servers', function (req, res) {
+    res.status(200).send({
+        "servers": [
+            {
+                "region": "US",
+                "map": {
+                    "id": (Number(parsedJSON.maps.maps.filter(map => map.name === mapName)[0].id) || 999999),
+                    "name": (mapName || 'Example')
+                },
+                "name": "Mixed",
+                "playing": (playersNumber || '-1')
+            }
+        ]
+    });
+});
+server.use(middlewares);
+server.use(router);
+server.listen(3000, () => {
+    console.log('JSON Server is running');
+});
+
+request(options, function (error, response, body) {
     if (!error) {
-        mapName = response.contents[0].innerText;
-        playersNumber = response.contents[1].innerText;
-        server.get('/servers', function (req, res) {
-            res.status(200).send({
-                "servers": [
-                    {
-                        "region": "US",
-                        "map": {
-                            "id": Number(parsedJSON.maps.maps.filter(map => map.name === mapName)[0].id),
-                            "name": mapName
-                        },
-                        "name": "Mixed",
-                        "playing": playersNumber
-                    }
-                ]
-            });
-        });
-        server.use(middlewares);
-        server.use(router);
-        server.listen(3000, () => {
-            console.log('JSON Server is running');
-        });
+        mapName = body.contents[0].innerText;
+        playersNumber = body.contents[1].innerText;
     }
+    else console.log(error);
 });
 
 setInterval(function () {
-    getJSON('https://cloudquery-phjvnjk3w.now.sh/query?url=https%3A%2F%2Fstratus.network%2Fplay&selectors=*:nth-child(5)%20*:nth-child(2)%20%3E%20*:nth-child(1)%20%3E%20*%20%3E%20*%20%3E%20*,*:nth-child(2)%20%3E%20*:nth-child(5)%20%3E%20*%20%3E%20*%20%3E%20*:nth-child(1)%20%3E%20*:nth-child(2)', function (error, response) {
+    request(options, function (error, response, body) {
         if (!error) {
-            mapName = response.contents[0].innerText;
-            playersNumber = response.contents[1].innerText;
+            mapName = body.contents[0].innerText;
+            playersNumber = body.contents[1].innerText;
         }
+        else console.log(error);
     });
 }, 10000);
